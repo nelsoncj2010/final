43d460c04bbc5cd4c5204fe2fb429ecec46beab3
Taylor Cathcart <taylor.cathcart@kellogg.northwestern.edu>
2021-06-11 20:45:46 -0500

    push

--- netlify/functions/pull_restaurants.js
+++ netlify/functions/pull_restaurants.js
@@ -9,9 +9,7 @@ exports.handler = async function(e) {
 
     // query restaurants
     let restaurantsQuery = await db.collection(`restaurants`).where(`user`, `==`, qsParams.user).get()
-    console.log(restaurantsQuery)
     let restaurants = restaurantsQuery.docs
-    console.log(restaurants)
 
     if (restaurants) {
 
@@ -32,17 +30,19 @@ exports.handler = async function(e) {
             // query dishes related to this restaurant
             let dishesQuery = await db.collection(`dishes`).where(`user`, `==`, qsParams.user).where(`restaurant`, `==`, rId)
             let dishes = dishesQuery.docs
-    
-            // loop through related dishes
-            for (let j = 0; j < dishes.length; j++) {
-                // pull dishes data and insert into dishes array
-                let dId = dishes[j].id
-                let dData = dishes[j].data().dish
-    
-                // append dish string to the dishes array in the restaurant JSON
-                rJson.dishes.push(dData)
-            }
 
+            // if there are any
+            if (dishes) {
+                // loop through related dishes
+                for (let j = 0; j < dishes.length; j++) {
+                    // pull dishes data and insert into dishes array
+                    let dId = dishes[j].id
+                    let dData = dishes[j].data().dish
+        
+                    // append dish string to the dishes array in the restaurant JSON
+                    rJson.dishes.push(dData)
+                }
+            }
     }
 
         returnValue.push(rJson)
